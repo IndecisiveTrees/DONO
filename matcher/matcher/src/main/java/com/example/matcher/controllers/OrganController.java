2bd28282d5876f3112a42a4c71fa5ffb190f0880
Subhajeet Lahiri <subhajeetlahiri@gmail.com>
2023-12-01 17:31:10 +0530

    Matching algo added

--- matcher/matcher/src/main/java/com/example/matcher/controllers/OrganController.java
+++ matcher/matcher/src/main/java/com/example/matcher/controllers/OrganController.java
@@ -2,8 +2,7 @@ package com.example.matcher.controllers;
 
 import com.example.matcher.exceptions.ResourceNotFoundException;
 import com.example.matcher.models.*;
-import com.example.matcher.repositories.IdSequenceRepository;
-import com.example.matcher.repositories.OrganRepository;
+import com.example.matcher.repositories.*;
 import com.example.matcher.requests.OrganCreationRequest;
 import com.example.matcher.requests.OrganDetailsUpdateRequest;
 import com.example.matcher.requests.OrganStatusUpdateRequest;
@@ -11,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 @RequestMapping("/matcher/organ")
@@ -20,6 +19,60 @@ public class OrganController {
     OrganRepository organRepository;
     @Autowired
     IdSequenceRepository idSequenceRepository;
+    @Autowired
+    HospitalRepository hospitalRepository;
+    @Autowired
+    RecipientRepository recipientRepository;
+    @Autowired
+    DonorRepository donorRepository;
+
+    double giveDistance(Recipient recipient, double latOrgan, double longOrgan){
+        Hospital hospital = hospitalRepository.findById(recipient.getHospitalId()).get();
+        return org.apache.lucene.util.SloppyMath.haversinMeters(latOrgan, longOrgan, hospital.getLatitude(), hospital.getLongitude());
+    }
+
+    boolean matchBloodType(BloodGroup donor, BloodGroup recipient){
+        return true;
+    }
+
+    static int f(int severity, int viability){
+        return severity*severity*viability;
+    }
+    static class RecipientComparator implements Comparator<Recipient>{
+        public int compare(Recipient a, Recipient b){
+            return f(a.getSeverity(), a.getViability()) - f(b.getSeverity(), b.getViability());
+        }
+    }
+
+    public Recipient match(Organ organ){
+        ArrayList<Recipient> recipients = (ArrayList<Recipient>) recipientRepository.filterRecipientsByOrgan(organ.getOrganType().toString());
+
+        Hospital hospital = hospitalRepository.findById(organ.getHospitalId()).get();
+        Donor donor = donorRepository.findById(organ.getDonorId()).get();
+
+        //filter
+        recipients = new ArrayList<>(recipients
+                .stream()
+                .filter(recipient -> giveDistance(
+                        recipient,
+                        hospital.getLatitude(),
+                        hospital.getLongitude()
+                ) <= 10000)
+                .filter(recipient -> matchBloodType(
+                        donor.getBloodGroup(),
+                        recipient.getBloodGroup()
+                ))
+                .filter(recipient -> !organ.getDeclinedIds().contains(recipient.getId()))
+                .toList());
+
+        //sort
+        recipients.sort(new RecipientComparator());
+        if(recipients.isEmpty()){
+            throw new ResourceNotFoundException("No recipients found");
+        }
+        return recipients.get(0);
+    }
+
 
     @PostMapping
     public long createOrgan(@RequestBody OrganCreationRequest req){
@@ -30,7 +83,13 @@ public class OrganController {
 
         Organ organ = new Organ();
         organ.setId(nextId);
+        if(!donorRepository.existsById(req.getDonorId())){
+            throw new ResourceNotFoundException("Donor doesn't exist");
+        }
         organ.setDonorId(req.getDonorId());
+        if(!hospitalRepository.existsById(req.getHospitalId())){
+            throw new ResourceNotFoundException("Hospital doesn't exist");
+        }
         organ.setHospitalId(req.getHospitalId());
         organ.setOrganType(
                 OrganType.valueOf(req.getOrganType())
@@ -51,6 +110,12 @@ public class OrganController {
             throw new ResourceNotFoundException("Organ not found");
         }
         return organ.get();
+    }
+
+    @GetMapping("/{id}/match")
+    public Recipient getBestMatch(@PathVariable long id){
+        Organ organ = getOrganById(id);
+        return match(organ);
     }
 
     @DeleteMapping("/{id}")
